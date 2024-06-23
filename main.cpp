3b52cdbe3bb7d48c71cdd1cd949a1496e6c52090
wangchen <516760573@qq.com>
2024-06-23 20:01:25 +0800

    Cpp-& 引用

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
-#include <iostream>
+#include <stdio.h>
+
+/**
+ * 在子函数中修改主函数的变量值
+ * 使用C语言方式来实现
+ */
+void changeWithC(int *j) {
+    *j = *j + 1;
+}
+
+/**
+ * 在子函数中修改主函数的变量值
+ * 使用Cpp语言方式来实现
+ * 注意C语言中是不支持此种写法的
+ */
+void changeWithCpp(int &j) { // 形参中的&写法 称为 引用
+    j = j + 1;
+}
+
+void changeWithCppWithNoEffect(int j) {
+    j = j + 1;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int i = 10;
+    printf("modify with C before change: %d\n", i);
+    changeWithC(&i);
+    printf("modify with C after  change: %d\n", i);
+    changeWithCpp(i);
+    printf("modify with Cpp after  change: %d\n", i);
+    changeWithCppWithNoEffect(i);
+    printf("modify with CppWithNoEffect after  change: %d\n", i);
+
+    printf("-----------------------------------\n");
+
     return 0;
 }
