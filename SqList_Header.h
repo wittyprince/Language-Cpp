321307bb7b2d46a747bec62a3050ebaebc04a83e
wangchen <516760573@qq.com>
2024-06-30 11:42:40 +0800

    Cpp-线性表-顺序表-修改注释

--- SqList_Header.h
+++ SqList_Header.h
@@ -11,27 +11,29 @@
 #define MaxSize 50
 // typedef定义的别名后需要加分号;
 typedef int ElementType;
+// 顺序表
+// 线性表的实现方式之一, 说到实现, 所以顺序表是存储结构
+// 顺序表底层实现是基于 数组 实现的，又分为 静态分配 和 动态分配
 // 静态分配
-// 线性表(逻辑结构)的存储方式之一：顺序表
-// 定义顺序表的实现方式(注意顺序表是物理结构)
-// 底层是数组实现的
 typedef struct {
     ElementType data[MaxSize]; // 底层用数组来实现
     int length; // 当前顺序表中有多少个元素
-}SqList;
+} SqList;
 
 
 /**
  * 插入元素
- * L 要插入元素的顺序表(或数组，底层是用数组实现的)
- * position 要插入的位置, 从1开始 到 L.length+1结束
- * e 要插入的元素
+ * @param L         要插入元素的顺序表
+ * @param position  要插入的位置, 从1开始 到 L.length+1结束
+ *                  注意 顺序表中元素的位置 和 元素在数组中下标不一样,
+ *                      位置从1开始, 下标从0开始
+ * @param e         要插入的元素
  */
 bool ListInsert(SqList &L, int position, ElementType e) {
-    // 注意：这里是 L.length, 而不是MaxSize。
+    // 注意：这里是 L.length, 而不是MaxSize。MaxSize是用来判断是否溢出的。
     // 因为插入元素是顺序插入，即从第1个位置开始，到L.length+1结束之间的位置可以插入元素
     // 不会跳跃式的插入到L.length+2，因为中间不允许有空的位置出现
-    // position 1 <= position <= L.length + 1
+    // 所以position的取值范围为: 1 <= position <= L.length + 1
     if (position < 1 || position > L.length + 1) { // ① 判断要插入的位置是否合法
         return false;
     }
@@ -53,12 +55,13 @@ void printList(SqList L) {
     printf("\n");
 }
 
-/**
- * 删除顺序表中的元素
- * L 要删除元素的顺序表(或数组，底层是用数组实现的)
- * position 要删除的元素的位置
- * e 位置i上的元素的值(即要被删除的元素的值)
- */
+ /**
+  * 删除顺序表中的元素
+  * @param L            要删除元素的顺序表
+  * @param position     要删除的元素的位置
+  * @param e            位置position上的元素的值(即要被删除的元素的值)
+  * @return             true or false
+  */
 bool ListDelete(SqList &L, int position, ElementType &e) {
     // 合法性检查 position 1 <= position <= L.length
     if (position < 1 || position > L.length) { // ①判断要删除的位置是否合法
@@ -75,8 +78,8 @@ bool ListDelete(SqList &L, int position, ElementType &e) {
 
 /**
  * 查找某个元素的位置，找到就返回对应的位置，找不到返回0
- * L 要查找的顺序表
- * e 要查找的元素
+ * @param L     要查找的顺序表
+ * @param e     要查找的元素
  */
 int LocateElement(SqList L, ElementType e) {
     for (int i = 0; i < L.length; ++i) {
@@ -85,5 +88,16 @@ int LocateElement(SqList L, ElementType e) {
         }
     }
     return 0;
+}
+
+/**
+ * 查找第position个位置上的元素
+ */
+bool getElement(SqList L, int position, ElementType &e) {
+    if (position < 1 || position > L.length) {
+        return false;
+    }
+    e = L.data[position - 1];
+    return true;
 }
 #endif //LANGUAGE_CPP_SQLIST_HEADER_H
