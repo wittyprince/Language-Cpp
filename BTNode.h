65b9472b55a36c1933aa7fbfa99e4dda01576ee8
wangchen <516760573@qq.com>
2024-07-02 14:01:14 +0800

    Cpp-二叉树-变量重命名

--- BTNode.h
+++ BTNode.h
@@ -13,38 +13,38 @@ typedef char TreeElementType;
 // 二叉树节点
 typedef struct btNode {
     TreeElementType data;
-    struct btNode *lChild;
-    struct btNode *rChild;
+    struct btNode *left;
+    struct btNode *right;
 } BTNode;
 
 // 辅助队列? 辅助链表?
 // 用于临时存放 二叉树节点
 typedef struct btListNode {
-//    struct btNode *btNodePointer; // struct btNode *btNodePointer 与 BTNode *btNodePointer两种方式均可
-    BTNode *btNodePointer; // 用于存放某一节点的地址值
-    struct btListNode *nextBtListNode;
+//    struct btNode *data; // struct btNode *data 与 BTNode *btNodePointer两种方式均可
+    BTNode *data; // 用于存放某一节点的地址值
+    struct btListNode *next;
 } BTListNode;
 
 void preOrder(BTNode *btNode) {
     if (btNode != NULL) {
         putchar(btNode->data); // 等价于 printf("%c", btNode->data);
-        preOrder(btNode->lChild);
-        preOrder(btNode->rChild);
+        preOrder(btNode->left);
+        preOrder(btNode->right);
     }
 }
 
 void inOrder(BTNode *btNode) {
     if (btNode != NULL) {
-        inOrder(btNode->lChild);
+        inOrder(btNode->left);
         putchar(btNode->data);
-        inOrder(btNode->rChild);
+        inOrder(btNode->right);
     }
 }
 
 void postOrder(BTNode *btNode) {
     if (btNode != NULL) {
-        postOrder(btNode->lChild);
-        postOrder(btNode->rChild);
+        postOrder(btNode->left);
+        postOrder(btNode->right);
         putchar(btNode->data);
     }
 }
--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -15,13 +15,13 @@ int main() {
     char c;
     scanf("%c", &c);
     root->data = c;
-    root->lChild = NULL;
-    root->rChild = NULL;
+    root->left = NULL;
+    root->right = NULL;
 
     // 声明一个链表, btListNode指向当前链表的尾部, 用于后续尾插法入链表时使用
     BTListNode *btListNode = (BTListNode *) malloc(sizeof (BTListNode));
-    btListNode->btNodePointer = root;
-    btListNode->nextBtListNode = NULL;
+    btListNode->data = root;
+    btListNode->next = NULL;
     // 当前链表指针, 用于入链表时判断当前的BTNode节点
     BTListNode *currentBtListNode = btListNode;
     // scanf()的返回值是成功赋值的变量数量, 发生错误时返回EOF.
@@ -31,19 +31,19 @@ int main() {
         tmpBTNode->data = c;
         // 声明链表节点
         BTListNode *tmpListNode = (BTListNode *) malloc(sizeof (BTListNode));
-        tmpListNode->btNodePointer = tmpBTNode;
-        tmpListNode->nextBtListNode = NULL;
+        tmpListNode->data = tmpBTNode;
+        tmpListNode->next = NULL;
         // 尾插法 入链表(或入队列)
-        btListNode->nextBtListNode = tmpListNode;
-        btListNode = btListNode->nextBtListNode;
+        btListNode->next = tmpListNode;
+        btListNode = btListNode->next;
         // 判断是进入左子树还是右子树
-        BTNode *currentBtNode = currentBtListNode->btNodePointer;
-        if (currentBtNode->lChild == NULL) {
-            currentBtNode->lChild = tmpBTNode;
-        } else if (currentBtNode->rChild == NULL) {
-            currentBtNode->rChild = tmpBTNode;
+        BTNode *currentBtNode = currentBtListNode->data;
+        if (currentBtNode->left == NULL) {
+            currentBtNode->left = tmpBTNode;
+        } else if (currentBtNode->right == NULL) {
+            currentBtNode->right = tmpBTNode;
             // 右子树时要移动当前链表指针currentBtListNode
-            currentBtListNode = currentBtListNode->nextBtListNode;
+            currentBtListNode = currentBtListNode->next;
         }
     }
     // 前序遍历
